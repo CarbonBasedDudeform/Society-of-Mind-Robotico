c22c7e3970da990ac45a007c3003786c695a81c3
CarbonBasedDudeform <imdomdude@gmail.com>
2011-12-02 08:56:56 +0000

    added code to header and basic skeleton to cpp file

--- Observe.cpp
+++ Observe.cpp
@@ -1,9 +1,35 @@
 #include "Observe.h"
 
-Observe::Observe(void)
+Observe::Observe(int number_of_sensors)
 {
+	readings = new int[number_of_sensors][number_of_sensors];
+	NumOfSensors = number_of_sensors;
 }
 
 Observe::~Observe(void)
 {
+}
+
+ArActionDesired* Observe::fire(ArActionDesired currentDesired) {
+	m_desire.reset();
+
+	if (m_sonar == NULL) {
+		deactivate();
+		return NULL;
+	}
+
+	
+	return &m_desire;
+}
+
+
+void Observe::setRobot(ArRobot *robot) {
+	ArAction::setRobot(robot);
+	m_sonar = robot->findRangeDevice("sonar");
+	//myRobot is variable from aria, just using mine due to style
+	m_robot = myRobot;
+	if (robot == NULL) {
+		ArLog::log(ArLog::Terse, "automata: found no sonar therefore deactivating");
+		deactivate();
+	}
 }
--- Observe.h
+++ Observe.h
@@ -1,10 +1,22 @@
 #pragma once
+#include "Society of Mind.h"
 
-class Observe
+class Observe : public ArAction, public automata
 {
 public:
-	Observe(void);
+	Observe(int number_of_sensors);
 	~Observe(void);
+
+	virtual ArActionDesired *Observe::fire(ArActionDesired currentDesired);
+	void setRobot(ArRobot *robot);
+	bool AnalyseData();
+	void GetNewReadings();
+
 private:
 	static const int MAX_READING = 5000;
+	Stop* stop;
+	int* readings;
+	int NumOfSensors;
+	void StoreReadings();
+
 };
